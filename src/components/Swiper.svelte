37dea85aca95a4040d91edbaf8c87f85d577f768
Fabio Villavicencio <favm72@gmail.com>
2023-01-22 00:09:31 -0500

    first commit

---
+++ src/components/Swiper.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+</script>
+
+<div class="items">
+  <div class="item">ITEM 1</div>
+  <div class="item">ITEM 2</div>
+  <div class="item">ITEM 3</div>
+  <div class="item">ITEM 4</div>
+  <div class="item">ITEM 5</div>
+  <div class="item">ITEM 6</div>
+  <div class="item">ITEM 1</div>
+  <div class="item">ITEM 2</div>
+  <div class="item">ITEM 3</div>
+  <div class="item">ITEM 4</div>
+  <div class="item">ITEM 5</div>
+  <div class="item">ITEM 6</div>
+</div>
+
+<style>
+  .items {
+    background: orange;
+    display: flex;
+    overflow: auto;
+  }
+  .item {
+    white-space: nowrap;
+    min-width: 200px;
+    display: grid;
+    place-content: center;
+    border: 1px solid black;
+  }
+</style>
